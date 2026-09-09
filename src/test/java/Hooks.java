6938cbca6f9c0b1bc981bf51b34bcd1ffb33be6e
John Markakis <jomarkakis@gmail.com>
2026-09-09 20:04:06 +0300

    Fix Hooks

--- src/test/java/Hooks.java
+++ src/test/java/Hooks.java
@@ -2,26 +2,23 @@ import io.cucumber.java.After;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 
 public class Hooks {
 
-    public static WebDriver driver;
-
     @After
     public void tearDown(Scenario scenario) {
-        if (scenario.isFailed() && driver != null) {
+        if (scenario.isFailed() && LoginSteps.driver != null) {
             try {
-                byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+                byte[] screenshot = ((TakesScreenshot) LoginSteps.driver).getScreenshotAs(OutputType.BYTES);
                 scenario.attach(screenshot, "image/png", "Failure Screenshot");
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
 
-        if (driver != null) {
-            driver.quit();
-            driver = null;
+        if (LoginSteps.driver != null) {
+            LoginSteps.driver.quit();
+            LoginSteps.driver = null;
         }
     }
 }
